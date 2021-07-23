bc87e8b1e7d0ee6cb9c85e367fb6af834590c1e9
Md-Faizan-Alam <87686216+Md-Faizan-Alam@users.noreply.github.com>
2021-07-23 11:26:52 +0530

    Added new elements

--- Move.js
+++ Move.js
@@ -1,33 +1,61 @@
-
-let slider = document.getElementsByClassName("slider");
-let face = document.getElementsByClassName("face");
-function sin(a){
-    return(Math.sin((a*Math.PI)/180));
+// Making variables to access the elements and the input values
+let slider= document.getElementById("container");
+let angle = document.getElementsByClassName("angle");
+let square = document.getElementsByClassName("square");
+let x,y,z,n=6;
+// Making arrays of attributes to be assigned to the squares
+let colors = ["red", "blue", "green", "yellow", "violet", "orange"];
+let seeThrough = ["1", "1", "1", "1", "1", "1"];
+// Assigning the above mentioned attributes
+function defaultStyle(){
+    for(let i=0;i<n;++i){
+        square[i].style.backgroundColor= colors[i];
+        square[i].style.opacity= seeThrough[i];
+    }
 }
-function cos(a){
-    return(Math.cos((a*Math.PI)/180));
+// Dynamically aligning the components to the center
+function center(){
+    let hShift = screen.width;
+    if(hShift>400){
+        hShift = (hShift/2)-80;
+    }
+    else{
+        hShift = (hShift/2)-40;
+    }
+    for(let i=0 ; i<n ; ++i){
+        square[i].style.left = hShift+"px";
+    }
+    slider.style.left= (hShift-50)+"px";
 }
+// Function to produce integral values for z-index
 function setZ(r){
-    return (2+Math.floor(r*4));
+    return(2+Math.floor(r*4));
 }
-function move(){
-    let x=slider[0].value;
-    let y=slider[1].value;
-    let z=slider[2].value;
-    let r1 = (cos(x)*cos(y));
-    let r2 = sin(y);
-    let r3 = (sin(x)*cos(y));
-    face[0].style.zIndex = setZ(r1);
-    face[1].style.zIndex = setZ(-r1);
-    face[2].style.zIndex = setZ(r2);
-    face[3].style.zIndex = setZ(-r2);
-    face[4].style.zIndex = setZ(r3);
-    face[5].style.zIndex = setZ(-r3);
-    face[0].style.transform ="rotateZ("+z+"deg) rotateY("+y+"deg) rotateX("+x+"deg)";
-    face[1].style.transform ="rotateZ("+z+"deg) rotateY("+y+"deg) rotateX(180deg) rotateX("+x+"deg)";
-    face[2].style.transform ="rotateY(-90deg) rotateX("+z+"deg) rotateY("+y+"deg) rotateZ("+(-x)+"deg)";
-    face[3].style.transform ="rotateY(90deg) rotateX("+(-z)+"deg) rotateY("+y+"deg) rotateZ("+x+"deg)";
-    face[4].style.transform ="rotateX(-90deg) rotateY("+(-z)+"deg) rotateZ("+y+"deg) rotateX("+x+"deg)";
-    face[5].style.transform ="rotateX(90deg) rotateY("+z+"deg) rotateZ("+y+"deg) rotateX("+(-x)+"deg)";
+function cos(t){
+    return(Math.cos(t*(Math.PI)/180));
 }
-setInterval(move,1);
+function sin(t){
+    return(Math.sin(t*(Math.PI)/180));
+}
+// Assinging the required transformation to the respective squares
+function orient(){
+    x = angle[0].value;
+    y = angle[1].value;
+    z = angle[2].value;
+    square[0].style.zIndex = setZ(cos(x)*cos(y));
+    square[1].style.zIndex = setZ(-cos(x)*cos(y));
+    square[2].style.zIndex = setZ(sin(y));
+    square[3].style.zIndex = setZ(-sin(y));
+    square[4].style.zIndex = setZ(sin(x)*cos(y));
+    square[5].style.zIndex = setZ(-sin(x)*cos(y));
+    square[0].style.transform ="rotateZ("+z+"deg) rotateY("+y+"deg) rotateX("+x+"deg)";
+    square[1].style.transform ="rotateZ("+z+"deg) rotateY("+y+"deg) rotateX(180deg) rotateX("+x+"deg)";
+    square[2].style.transform ="rotateY(-90deg) rotateX("+z+"deg) rotateY("+y+"deg) rotateZ("+(-x)+"deg)";
+    square[3].style.transform ="rotateY(90deg) rotateX("+(-z)+"deg) rotateY("+y+"deg) rotateZ("+x+"deg)";
+    square[4].style.transform ="rotateX(-90deg) rotateY("+(-z)+"deg) rotateZ("+y+"deg) rotateX("+x+"deg)";
+    square[5].style.transform ="rotateX(90deg) rotateY("+z+"deg) rotateZ("+y+"deg) rotateX("+(-x)+"deg)";
+}
+// Executing all the functions defined above
+defaultStyle();
+setInterval(center, 1);
+setInterval(orient,1);
